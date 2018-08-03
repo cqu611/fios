4b5da8fc641d51fc3678208083c507b2e88e8a32
l0g0ut <root@l0g0ut.xin>
2018-08-03 04:06:46 +0000

    fio: add fio-script

---
+++ fiorw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+fio -filename=/dev/sdb4 \
+	-direct=1 \
+	-iodepth 1 \
+	-thread \
+	-rw=${1} \
+	-ioengine=psync \
+	-bs=16k \
+	-size=2G \
+	-numjobs=10 \
+	-runtime=60 \
+	-group_reporting \
+	-name=test-${1} \
+	| tee >> fio-${1}.result
+
+
